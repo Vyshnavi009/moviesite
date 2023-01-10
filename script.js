ea285cc5f487b04adc320353034ae5a46996dd49
P.Vyshnavi <94984934+Vyshnavi009@users.noreply.github.com>
2023-01-10 16:59:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ff22525f9de3f9254280ce6f40f508b8&page=1';
+  const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+  const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=ff22525f9de3f9254280ce6f40f508b8&query=";
+
+
+  const main = document.getElementById("section");
+  const form = document.getElementById("form");
+  const search = document.getElementById("query");
+
+returnMovies(APILINK)
+  function returnMovies(url){
+    fetch(url).then(res => res.json())
+    .then(function(data){
+    console.log(data.results);
+    data.results.forEach(element => {
+        const div_content = document.createElement('div');
+        div_content.setAttribute('class', 'content');
+        
+        const div_complete = document.createElement('div');
+        div_complete.setAttribute('class', 'complete');
+        
+        const div_card = document.createElement('div');
+        div_card.setAttribute('class', 'card');
+        
+        const image = document.createElement('img');
+        image.setAttribute('class', 'thumbnail');
+        image.setAttribute('id', 'image');
+        
+        const title = document.createElement('h3');
+        title.setAttribute('id', 'title');
+        
+        const center = document.createElement('center');
+  
+        title.innerHTML = `${element.title}`;
+        image.src = IMG_PATH + element.poster_path;
+  
+        center.appendChild(image);
+        div_content.appendChild(center);
+        div_content.appendChild(title);
+        div_card.appendChild(div_content);
+        div_complete.appendChild(div_card);
+  
+        main.appendChild(div_complete);
+    });
+  });
+  }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  main.innerHTML = '';
+
+  const searchItem = search.value;
+
+  if (searchItem) {
+    returnMovies(SEARCHAPI + searchItem);
+      //search.value = "";
+  }
+
+
+});
